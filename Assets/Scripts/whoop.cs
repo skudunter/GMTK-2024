08c7ddfc9f1d4f875f84383bcc49052d665db3dc
skudunter <danielgruttercpt@gmail.com>
2024-08-20 19:12:14 +0200

    feat updated things

--- Assets/Scripts/whoop.cs
+++ Assets/Scripts/whoop.cs
@@ -1,12 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class whoop : MonoBehaviour
 {
     public void enterGame(){
+        Debug.Log("entered  game");
         SceneManager.LoadScene("Game");
     }
 }
